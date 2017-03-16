1ed46a646af52a54c775cf24382579c7596adc55
ArmandAbrahamian <armand.abrahamian@gmail.com>
2017-03-16 11:05:15 -0700

    Initial Commit with Template.

---
+++ src/com/company/JobScheduler.java
@@ -0,0 +1,133 @@
+/*
+    Project #2: Job Scheduler
+    Description: Create three java classes to solve the job scheduling problem described.
+    Class: COMP496 ALG 11:00AM, Professor Schwartz
+    Team Members: Inkyu Park, Armand Abrahamian
+    Date: April 6, 2017
+ */
+
+package com.company;
+
+import java.util.*;
+
+public class JobScheduler
+{
+
+        private int nJobs;
+        private Job[]  jobs;
+
+        public JobScheduler( int[] joblength, int[] deadline, int[] profit)
+        {
+            //Set nJobs
+            //Fill jobs array. The kth job entered has JobNo = k;
+        }
+
+        public void printJobs()  //prints the array jobs
+        {
+
+        }
+
+        //Brute force. Try all n! orderings. Return the schedule with the most profit
+        public Schedule bruteForceSolution()
+        {
+
+        }
+
+
+        public Schedule makeScheduleEDF()
+        //earliest deadline first schedule. Schedule items contributing 0 to total profit last
+        {
+
+        }
+
+        public Schedule makeScheduleSJF()
+        //shortest job first schedule. Schedule items contributing 0 to total profit last
+        {
+
+        }
+
+        public Schedule makeScheduleHPF()
+        //highest profit first schedule. Schedule items contributing 0 to total profit last
+        {
+
+        }
+
+
+        public Schedule newApproxSchedule() //Your own creation. Must be <= O(n3)
+        {
+
+        }
+
+    }//end of JobScheduler class
+
+    //---------------------------Include Job and Schedule classes in JobScheduler. java-----------------------------
+    class Job
+    {
+        int jobNumber;
+        int length;
+        int deadline;
+        int profit;
+        int start;
+        int finish;
+
+
+        public Job( int jn , int len, int d, int p)
+        {
+            jobNumber = jn; length = len; deadline = d;
+            profit = p;  start = -1;  finish = -1;
+        }
+
+
+        public String toString()
+        {
+            return "#" + jobNumber + ":(" + length + ","
+                    + deadline + "," + profit +
+                    "," + start + "," + finish + ")";
+        }
+
+    }//end of Job class
+
+
+
+    // ----------------------------------------------------
+    class Schedule
+    {
+        ArrayList<Job> schedule;
+        int profit;
+
+        public Schedule()
+        {
+            profit = 0;
+            schedule = new ArrayList<Job>();
+        }
+
+        public void add(NPJob job)
+        {
+
+        }
+
+
+        public int getProfit()
+        {
+
+        }
+
+        public String toString()
+        {
+            String s = "Schedule Profit = " + profit ;
+            for(int k = 0 ; k < schedule.size(); k++)
+            {
+                s = s + "\n"  + schedule.get(k);
+
+            }
+
+            return s;
+        }
+    }// end of Schedule class
+
+
+    public static void main(String[] args)
+    {
+	    // write your code here
+    }
+}
